1b8d4cb3e91efd5cef7fe314ed9e4f8235c256b1
Marlus Vinicius <marlusvinicius2014@outlook.com>
2022-04-03 15:27:40 -0300

    Implements nutrition without UI

---
+++ Assets/Code/Nutrition.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Code
+{
+    public class Nutrition : MonoBehaviour
+    {
+        [SerializeField] private float nutritionDecayPeriod = 1.0f; 
+        [Range(0, 1)] [SerializeField] private float nutritionDecayFactor = 0.1f;
+
+        private float _currentNutrition;
+        
+        private const float MAXNutrition = 1.0f;
+
+        private void Awake()
+        {
+            _currentNutrition = MAXNutrition;
+        }
+
+        private void Start()
+        {
+            StartCoroutine(Hunger());
+        }
+
+        private void Update()
+        {
+            if (_currentNutrition <= 0)
+            {
+                Die();
+            }
+        }
+
+        /// <summary>
+        /// Loses *nutritionDecayFactor* each *nutritionDecayPeriod* seconds
+        /// </summary>
+        /// <returns> it's a COROUTINE </returns>
+        private IEnumerator Hunger()
+        {
+            var period = new WaitForSeconds(nutritionDecayPeriod);
+            while (true)
+            {
+                yield return period;
+                _currentNutrition -= nutritionDecayFactor;
+            }
+        }
+
+        //Deve ficar numa classe separada
+        void Die()
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+}
