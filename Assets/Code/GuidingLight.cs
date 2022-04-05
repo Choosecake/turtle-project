94c195fa11ecc127586055b5fd896a6d2881c208
Choosecake <imchoosecake@gmail.com>
2022-04-05 16:02:28 -0300

    creates guiding light script

---
+++ Assets/Code/GuidingLight.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.GlobalIllumination;
+
+public class GuidingLight : MonoBehaviour
+{
+    [SerializeField] private GameObject[] waypoints;
+    [SerializeField] private Transform player;
+    [SerializeField] private float minDistance = 15f;
+    [SerializeField] private GameObject pointLight;
+    [Range(1.0f, 250.0f)] [SerializeField] private float pointLightSpeed;
+    private int currentWaypoint;
+
+    private void Start()
+    {
+        currentWaypoint = 0;
+        pointLight.transform.position = waypoints[currentWaypoint].transform.position;
+    }
+
+    private void Update()
+    {
+        pointLight.transform.position = Vector3.Lerp(
+            pointLight.transform.position,
+            waypoints[currentWaypoint].transform.position,
+            pointLightSpeed * Time.deltaTime);
+        
+        if (Vector3.Distance(waypoints[currentWaypoint].transform.position, player.position) < minDistance)
+        {
+            if (currentWaypoint < waypoints.Length) currentWaypoint += 1;
+        }
+    }
+}
