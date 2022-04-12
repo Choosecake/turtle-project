13d806e4994d558c3aa4083b0097b077db5397db
Choosecake <imchoosecake@gmail.com>
2022-04-12 13:26:22 -0300

    implements shark spawn feature and resets scene upon collision with player

--- Assets/Code/SharkBehaviour.cs
+++ Assets/Code/SharkBehaviour.cs
@@ -1,16 +1,35 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.SearchService;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SharkBehaviour : MonoBehaviour
 {
-    [SerializeField] private Transform turtle;
+    private GameObject turtle;
+    private Collider turtleCollider;
     [SerializeField] private float movementSpeed;
+
+    private void Start()
+    {
+        // nojo nojento fix this
+        turtle = GameObject.Find("PlayerTurtle");
+        turtleCollider = turtle.GetComponent<Collider>();
+    }
 
     private void Update()
     {
-        transform.position = Vector3.Lerp(transform.position, turtle.position, movementSpeed * Time.deltaTime);
-        transform.LookAt(turtle);
+        transform.position = Vector3.Lerp(transform.position, turtle.transform.position, movementSpeed * Time.deltaTime);
+        transform.LookAt(turtle.transform);
+    }
+
+    // criar classe separada
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other == turtleCollider)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
     }
 }
--- Assets/Code/SharkSpawn.cs
+++ Assets/Code/SharkSpawn.cs
@@ -1,7 +1,46 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using UnityEngine;
+using Debug = UnityEngine.Debug;
 
 public class SharkSpawn : MonoBehaviour
 {
+    [SerializeField] private GameObject boundary;
+    [SerializeField] private GameObject shark;
+    private Collider turtleCollider;
+    private Collider boundaryCollider;
+    private Vector3 position;
+    private float sharkDistance = 250.0f;
+    private bool hasSharkSpawned = false;
+
+    private void Start()
+    {
+        boundaryCollider = boundary.GetComponent<Collider>();
+    }
+
+    private void Update()
+    {
+        Vector3 direction = transform.position - boundary.transform.position;
+        float angle = Vector3.Angle(direction, boundary.transform.forward);
+
+        if (transform.position.x < 0)
+        {
+            position = Quaternion.Euler(0, -angle, 0) * Vector3.forward * sharkDistance;
+        }
+        else
+        {
+            position = Quaternion.Euler(0, angle, 0) * Vector3.forward * sharkDistance;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other == boundaryCollider && !hasSharkSpawned)
+        {
+            Instantiate(shark, position, transform.rotation);
+            hasSharkSpawned = true;
+        }
+    }
 }
