15b818949f8f5f6cb1e98e414bde5123db21c501
Marlus Vinicius <marlusvinicius2014@outlook.com>
2022-04-22 23:56:07 -0300

    Finalizes the whole start-to-end cycle and message

--- Assets/Code/UI/TransitionScreen.cs
+++ Assets/Code/UI/TransitionScreen.cs
@@ -1,18 +1,26 @@
 ﻿using System;
 using System.Collections;
+using System.Threading.Tasks;
 using Code;
 using DG.Tweening;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using TMPro;
 
 namespace UI
 {
     public class TransitionScreen : MonoBehaviour
     {
         [SerializeField] private TurtleVitalSystems _vitalSystems;
-        [SerializeField] private float fadeTime = 1.0f;
+        [SerializeField] private float fadeInTime = 1.0f;
         [SerializeField] private float delayTime = 0.5f;
+        [SerializeField] private float fadeOutTime = 1.0f;
+        [Header("Message")] 
+        [SerializeField] private string[] messageContent;
+        [SerializeField] private TextMeshProUGUI messageText;
+        [SerializeField] private float textFadeInTime;
+        [SerializeField] private float readingDelayTime;
 
         private Image _blackoutScreen;
 
@@ -26,18 +34,51 @@ namespace UI
             _vitalSystems.OnTurtleDeath += FadeInCoroutine;
         }
 
+        void Start()
+        {
+            _blackoutScreen.color = Color.black;
+            StartCoroutine(BackgroundFadeOut());
+        }
+
         private void OnDisable()
         {
             _vitalSystems.OnTurtleDeath -= FadeInCoroutine;
         }
         
-        private IEnumerator FadeIn()
+        private IEnumerator BackgroundFadeIn()
         {
-            Tween fadeTween = _blackoutScreen.DOFade(1, fadeTime);
-            yield return new WaitForSeconds(fadeTime+delayTime);
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            Tween fadeTween = _blackoutScreen.DOFade(1, fadeInTime);
+            yield return new WaitForSeconds(fadeInTime+delayTime);
+            StartCoroutine(MessageFadeCycle());
+        }
+        private void FadeInCoroutine() => StartCoroutine(BackgroundFadeIn());
+
+        private IEnumerator BackgroundFadeOut()
+        {
+            _blackoutScreen.DOFade(0, fadeOutTime);
+            yield return null;
+        }
+        
+        private async void BackgroundFadeIn_Async()
+        {
+            Tween fadeTween = _blackoutScreen.DOFade(1, fadeInTime);
+            await fadeTween.AsyncWaitForCompletion();
+            // SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
         }
 
-        private void FadeInCoroutine() => StartCoroutine(FadeIn());
+        private IEnumerator MessageFadeCycle()
+        {
+            var readingYield = new WaitForSeconds(textFadeInTime + readingDelayTime);
+            var fadeCompletionYield = new WaitForSeconds(textFadeInTime);
+            foreach (var subMessage in messageContent)
+            {
+                messageText.text = subMessage;
+                Tween fadeTween = messageText.DOFade(1, textFadeInTime);
+                yield return readingYield;
+                fadeTween = messageText.DOFade(0, textFadeInTime);
+                yield return fadeCompletionYield;
+            }
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
     }
 }
