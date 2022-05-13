da6488b6adbecb79eb4cf6c6f2fc9a50808d5fbe
Marlus Vinicius <marlusvinicius2014@outlook.com>
2022-05-13 19:04:32 -0300

    Implements singleton base script and start creating pausemenu ui

---
+++ Assets/Code/GameplayManager.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+using Utilities;
+
+namespace Code
+{
+    public class GameplayManager : MonoSingleton<GameplayManager>
+    {
+        private float defaultTime = 1f;
+        private bool isPaused;
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                TogglePause();
+            }
+        }
+
+        public bool IsPaused
+        {
+            get => isPaused;
+            set
+            {
+                isPaused = value;
+                if (value)  OnGamePause?.Invoke();
+            }
+        } 
+        public Action OnGamePause;
+
+        private void TogglePause()
+        {
+            if (!IsPaused)
+            {
+                defaultTime = Time.timeScale;
+                Time.timeScale = 0f;
+                IsPaused = true;
+            }
+            else
+            {
+                Time.timeScale = defaultTime;
+                IsPaused = false;
+            }
+        }
+    }
+}
