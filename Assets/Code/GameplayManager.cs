dd47df6ccd9ff90d4e874dc692e795affb0cff29
Marlus Vinicius <marlusvinicius2014@outlook.com>
2022-05-21 22:46:23 -0300

    Bug fixed

--- Assets/Code/GameplayManager.cs
+++ Assets/Code/GameplayManager.cs
@@ -6,15 +6,25 @@ namespace Code
 {
     public class GameplayManager : MonoSingleton<GameplayManager>
     {
+        [SerializeField] private TurtleVitalSystems playerVitalSystems;
+
         private float defaultTime = 1f;
         private bool isPaused;
 
         public Action OnGamePause;
         public Action OnGameResume;
 
+        void Start()
+        {
+            if (playerVitalSystems == null)
+            {
+                playerVitalSystems = FindObjectOfType<TurtleVitalSystems>();
+            }
+        }
+
         private void Update()
         {
-            if (Input.GetKeyDown(KeyCode.Escape))
+            if (Input.GetKeyDown(KeyCode.Escape) && !playerVitalSystems.IsDead)
             {
                 TogglePause();
             }
