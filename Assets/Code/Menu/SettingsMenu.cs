6ea69df9d699a6b94bc5f71e691992fe05457cf5
Choosecake <imchoosecake@gmail.com>
2022-05-24 15:50:08 -0300

    makes sensitivity adjustment

--- Assets/Code/Menu/SettingsMenu.cs
+++ Assets/Code/Menu/SettingsMenu.cs
@@ -1,13 +1,33 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class SettingsMenu : MonoBehaviour
 {
+    [SerializeField] private Slider mouseSensitivitySlider;
     public AudioMixer audioMixer;
+
+    private void Start()
+    {
+        if (PlayerPrefs.HasKey("Sensitivity"))
+        { 
+            mouseSensitivitySlider.value = PlayerPrefs.GetFloat("Sensitivity");
+        }
+    }
+
+    public void SetSensitivity(float sensitivity)
+    {
+        if (!Application.isPlaying) return;
+        
+        PlayerPrefs.SetFloat("Sensitivity", sensitivity);
+    }
+    
     public void SetVolume(float volume)
     {
         audioMixer.SetFloat("Volume", volume);
     }
+
 }
