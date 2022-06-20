a21a547015663aeb7d3c861b0e7265996c858966
Marlus Vinicius <marlusvinicius2014@outlook.com>
2022-06-19 23:37:05 -0300

    Starts correcting level reset

---
+++ Assets/Code/Managers/GeneralDataStorage.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace Code
+{
+    [CreateAssetMenu(fileName = "General Data Storage", menuName = "General Data Storage")]
+    public class GeneralDataStorageSO : ScriptableObject
+    {
+        public int currentLevel = 0;
+    }
+}
